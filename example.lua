719110037bbd3527f8e172713e8bc808e8f376b7
J0se-j <babybear.wdw@gmail.com>
2026-06-27 00:19:27 -0400

    Refactor UI code and update library reference
    Updated the library reference and modified the UI initialization. Removed unused sections and buttons for a cleaner interface.

--- example.lua
+++ example.lua
@@ -1,54 +1,11 @@
-local library = loadstring(game:HttpGet("https://github.com/GoHamza/AppleLibrary/blob/main/main.lua?raw=true"))()
+local AppleUi = loadstring(game:HttpGet("https://raw.githubusercontent.com/J0se-j/AppleLibrary/refs/heads/main/main.lua"))()
 
-local window = library:init("Titlebar", true, Enum.KeyCode.RightShift, true)
+-- Usage: init(Title, SplashEnabled, Keybind, DeletePrevious, ThemeColor)
+local Window = AppleUi:init("My Modern Hub", true, Enum.KeyCode.RightControl, true, "purple")
 
-window:Divider("I'm a divider!")
+local MainSection = Window:Section("Main")
+MainSection:Select()
 
-local sectionA = window:Section("Test Elements")
-
-sectionA:Divider("I'm another divider!")
-
-sectionA:Button("Click me!", function() 
-    print("Button clicked.")
+MainSection:Button("Click Me", function()
+    print("Button clicked!")
 end)
-
-sectionA:Label("Lorem ipsum dolor sit amet.")
-
-sectionA:Switch("Switch me!", false, function(a) 
-    print(a) 
-end)
-
-sectionA:TextField("Enter text here!", "Enter text here...", function(a)
-    print(a) 
-end)
-
-window:Divider("Just dividin'")
-
-local sectionB = window:Section("Test Notifications")
-
-sectionB:Divider("Dividers are cool!")
-
-sectionB:Button("Temporary Notification", function() 
-    window:TempNotify("Be careful!", "We are going to beat you up.", "rbxassetid://12608259004") 
-end)
-
-sectionB:Button("Notification 1", function() window:Notify("Hello!", "I am notification", "Button1", "rbxassetid://12608259004", 
-    function() 
-        print(1) 
-    end) 
-end)
-
-sectionB:Button("Notification 2", function() window:Notify2("Hello!", "I am notification", "Button 1", "Button 2", "rbxassetid://12608259004",
-    function() 
-        print(1) 
-    end,
-    function() 
-        print(2) 
-    end)
-end)
-
-window:GreenButton(function() 
-    print("You clicked the green button!")
-end)
-
-sectionA:Select()
