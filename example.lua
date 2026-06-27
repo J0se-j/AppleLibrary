15a83ccc829363785e41ad3b184fc27a0ea521a7
J0se-j <babybear.wdw@gmail.com>
2026-06-27 00:32:22 -0400

    Refactor AppleUi usage and button functionality

--- example.lua
+++ example.lua
@@ -1,11 +1,14 @@
 local AppleUi = loadstring(game:HttpGet("https://raw.githubusercontent.com/J0se-j/AppleLibrary/refs/heads/main/main.lua"))()
 
--- Usage: init(Title, SplashEnabled, Keybind, DeletePrevious, ThemeColor)
-local Window = AppleUi:init("My Modern Hub", true, Enum.KeyCode.RightControl, true, "purple")
+local Window = AppleUi:init("Test", false, nil, false, "purple")
 
-local MainSection = Window:Section("Main")
-MainSection:Select()
+local Main = Window:Section("Main")
+Main:Select()
 
-MainSection:Button("Click Me", function()
-    print("Button clicked!")
+Main:Button("Click me", function()
+    print("works")
+end)
+
+Main:Toggle("Test toggle", false, function(v)
+    print(v)
 end)
